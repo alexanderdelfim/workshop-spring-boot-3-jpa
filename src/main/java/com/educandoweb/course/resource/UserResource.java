6bc75ff73d5e3aec48949a1018d94f062ff90337
Alexander Delfim <alexanderdelfimjr11@gmail.com>
2023-06-21 22:13:23 -0300

    feat: creating the user resourse

---
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Alexander", "alexamder@example.com", "999999999", "1234ABCD");
+        return ResponseEntity.ok().body(user);
+    }
+}
